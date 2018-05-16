28cb03258f5daf7c976e3e2cc5e7b2f6bd67fd08
Brian Ruff <brff19@icloud.com>
2018-05-16 19:11:00 -0400

    added mongoose connection to mongodb

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // module.exports allows us to connect outside of the file
+    mongoURI: 'mongodb://brff19:123456@ds257495.mlab.com:57495/brianruff-devconnector'
+};
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+// DB config, pulls monoURI object property from config/keys.js files
+const db = require('./config/keys').mongoURI;
+
+// connect to mongodb via mongoose
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error(Error(err)));
 
 app.get('/', (req, res) => res.send('Hello, World!!'));
 
