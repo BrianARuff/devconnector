1c16ff4f6640cc321243cb933b36b8b499e9c8a9
Brian Ruff <brff19@icloud.com>
2018-05-22 20:00:26 -0400

    validation for username length

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// load input validation
+const validateRegisterInput = require('../../validation/register');
+
 // load user model with mongoose
 const User = require('../../models/User');
 
@@ -18,6 +21,11 @@ router.get('/test', (req, res) => res.json({msg: "Users works"}));
 // @description, register user
 // @access,      Public route
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+    // check validation
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
     User.findOne({email: req.body.email})
         .then(user => {
             if(user) {
